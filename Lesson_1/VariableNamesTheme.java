26bb2d96342ce73a6206f6bdc104099694798bab
Кристина Кириленко <kristina.kirilenko@example.com>
2026-01-12 17:57:35 +0300

    HM

--- Lesson_1/VariableNamesTheme.java
+++ Lesson_1/VariableNamesTheme.java
@@ -1,178 +1,175 @@
-@SuppressWarnings({"checkstyle:Indentation", "checkstyle:LineLength", "checkstyle:RightCurly"})
+package Lesson_1;
+
 public class VariableNamesTheme {
-   @SuppressWarnings({"checkstyle:MatchXpath", "checkstyle:SingleSpaceSeparator", "checkstyle:ParenPad", "checkstyle:LocalVariableName", "checkstyle:WhitespaceAround", "checkstyle:AbbreviationAsWordInName", "checkstyle:RegexpMultiline", "checkstyle:EmptyLineSeparator"})
-   static void main() {
-       // цифра
-       int number = 1;
-       System.out.println("цифра = " + number);
+    static void main() {
+        // цифра
+        int number = 1;
+        System.out.println("цифра = " + number);
 
-       // сумма чисел
-       int sum = 12 + 5;
-       System.out.println("сумма чисел = " + sum);
+        // сумма чисел
+        int sum = 12 + 5;
+        System.out.println("сумма чисел = " + sum);
 
-       // максимальное число
-       int max = 5;
-       System.out.println("максимальное число = " + max);
+        // максимальное число
+        int max = 5;
+        System.out.println("максимальное число = " + max);
 
-       System.out.println();
+        System.out.println();
 
-       //количество десятков
-       int tensCount = 10;
-       System.out.println("количество десятков =" + tensCount);
+        // количество десятков
+        int tensCount = 10;
+        System.out.println("количество десятков =" + tensCount);
 
-       //вес собаки
-       int dogsWeight = 6;
-       System.out.println("вес собаки = " + dogsWeight);
+        // вес собаки
+        int dogsWeight = 6;
+        System.out.println("вес собаки = " + dogsWeight);
 
-       //исходное число
-       int originalNumber = 1;
-       System.out.println("исходное число = " + originalNumber);
+        // исходное число
+        int originalNumber = 1;
+        System.out.println("исходное число = " + originalNumber);
 
-       System.out.println();
+        System.out.println();
 
-       //процент по вкладу
-       int percent = 10;
-       System.out.println("процент по вкладу = " + percent);
+        // процент по вкладу
+        int percent = 10;
+        System.out.println("процент по вкладу = " + percent);
 
-       // символ &
-       String ampersand  = "&";
-       System.out.println("символ " + ampersand );
+        // символ &
+        String ampersand = "&";
+        System.out.println("символ " + ampersand);
 
-       //код ошибки
+        // код ошибки
+        int error = 567893894;
+        System.out.println("код ошибки = " + error);
 
-       int error = 567893894;
-       System.out.println("код ошибки = " + error);
+        System.out.println();
 
-       System.out.println();
+        // тип сообщения
+        String string = "тип сообщения";
+        System.out.println("тип сообщения = " + string);
 
-       //тип сообщения
-       String string = "тип сообщения";
-       System.out.println("тип сообщения = " + string);
+        // число нулей
+        int zerosCount = 3;
+        System.out.println("число нулей = " + zerosCount);
 
-       //число нулей
-       int zerosCount  = 3;
-       System.out.println("число нулей = " + zerosCount );
+        // уникальное число
+        int uniqueNumber = 6;
+        System.out.println("уникальное число = " + uniqueNumber);
 
-       //уникальное число
-       int uniqueNumber  = 6;
-       System.out.println("уникальное число = " + uniqueNumber );
+        System.out.println();
 
-       System.out.println();
+        // случайное число
+        int randomNumber = 3;
+        System.out.println("случайное число = " + randomNumber);
 
-       // случайное число
-       int randomNumber = 3;
-       System.out.println("случайное число = " + randomNumber);
+        // математическое выражение
+        String mathematicalExpression = "8+9-8";
+        System.out.println("математическое выражение " + mathematicalExpression);
 
-       //математическое выражение
-       String mathematicalExpression ="8+9-8";
-       System.out.println("математическое выражение " + mathematicalExpression);
+        // счет в игре
 
-       //счет в игре
+        String account = "3:0";
+        System.out.println("счет в игре = " + account);
 
-       String account = "3:0";
-       System.out.println("счет в игре = " + account);
+        System.out.println();
 
-       System.out.println();
+        // максимальная длина
+        int maximum = 98;
+        System.out.println("максимальная длинна = " + maximum);
 
-       //максимальная длина
-       int maximum = 98;
-       System.out.println("максимальная длинна = " + maximum);
+        // пункт меню
+        String menu = "кофе";
+        System.out.println("пункт меню = " + menu);
 
-       //пункт меню
-       String menu = "кофе";
-       System.out.println("пункт меню = " + menu);
+        // стоимость кофе
+        double coffee = 234.5;
+        System.out.println("стоимость кофе = " + coffee);
 
-       // стоимость кофе
-       double coffee = 234.5;
-       System.out.println("стоимость кофе = " + coffee);
+        System.out.println();
 
-       System.out.println();
+        // начальная дата
+        int data = 6;
+        System.out.println("начальная дата = " + data);
 
-       // начальная дата
-       int data = 6;
-       System.out.println("начальная дата = " + data);
+        // окончание диапазона
+        int rangeEnd = 99;
+        System.out.println("окончание диапазона =" + rangeEnd);
 
-       //окончание диапазона
-       int rangeEnd  = 99;
-       System.out.println("окончание диапазона =" + rangeEnd );
+        // имя работника месяца
+        String employee = "Иван";
+        System.out.println("имя работника месяца = " + employee);
 
-       //имя работника месяца
-       String employee = "Иван";
-       System.out.println("имя работника месяца = " + employee);
+        System.out.println();
 
-       System.out.println();
+        // название электронной книги
+        String book = "Весна";
+        System.out.println("название электронной книги = " + book);
 
-       //название электронной книги
-       String book = "Весна";
-       System.out.println("название электронной книги = " + book);
+        // размер
+        int size = 57;
+        System.out.println("размер = " + size);
 
-       //размер
-       int size = 57;
-       System.out.println("размер = " + size);
+        // вместимость
+        double capacity = 57.356;
+        System.out.println("вместимость = " + capacity);
 
-       //вместимость
-       double capacity = 57.356;
-       System.out.println("вместимость = " + capacity);
+        System.out.println();
 
-       System.out.println();
+        // счетчик
+        int counter = 0;
+        System.out.println("счетчик = " + counter);
 
-       //счетчик
-       int counter = 0;
-       System.out.println("счетчик = " + counter);
+        // путь до файла
+        String way = "C:\\startjava\\Lesson__1\\src";
+        System.out.println("путь до файла = " + way);
 
-       //путь до файла
-       String way = "C:\\startjava\\Lesson__1\\src";
-       System.out.println("путь до файла = " + way);
+        // количество чисел в строке
+        int quantity = 10;
+        System.out.println("количество чисел в строке = " + quantity);
 
-       //количество чисел в строке
-       int quantity = 10;
-       System.out.println("количество чисел в строке = " + quantity);
+        System.out.println();
 
-       System.out.println();
+        // сотни равны?
+        boolean equalityNumber = false;
+        System.out.println("сотни равны? " + equalityNumber);
 
-       //сотни равны?
-       boolean equalityNumber = false;
-       System.out.println("сотни равны? " + equalityNumber);
+        // компьютер включен?
+        boolean computer = true;
+        System.out.println("компьютер включен? " + computer);
 
-       //компьютер включен?
-       boolean computer = true;
-       System.out.println("компьютер включен? " + computer);
+        // есть равные цифры?
+        boolean numbers = true;
+        System.out.println("есть равные цифры? " + numbers);
 
-       //есть равные цифры?
-       boolean numbers = true;
-       System.out.println("есть равные цифры? " + numbers);
+        System.out.println();
 
-       System.out.println();
+        // служба создана?
+        boolean service = false;
+        System.out.println("служба создана? " + service);
 
-       //служба создана?
-       boolean service = false;
-       System.out.println("служба создана? " + service);
+        // файл пустой?
+        boolean file = false;
+        System.out.println("файл пустой? " + file);
 
-       // файл пустой?
-       boolean file = false;
-       System.out.println("файл пустой? " + file);
+        // соединение активное?
+        boolean connection = true;
+        System.out.println("соединение активное? " + connection);
 
-       //соединение активное?
-       boolean connection= true;
-       System.out.println("соединение активное? " + connection);
+        System.out.println();
 
-       System.out.println();
+        // новый?
+        boolean theNew = false;
+        System.out.println("новый ?" + theNew);
 
-       //новый?
-       boolean the_new = false;
-       System.out.println("новый ?" + the_new);
+        // электронная почта действительная?
+        boolean email = true;
+        System.out.println("электронная почта действительная? " + email);
 
-       //электронная почта действительная?
-       boolean email = true;
-       System.out.println("электронная почта действительная? " + email);
+        // имеются уникальные строки?
+        boolean uniqueStrings = true;
+        System.out.println("имеются уникальные строки? " + uniqueStrings);
 
-       //имеются уникальные строки?
-       boolean unique_strings = true;
-       System.out.println("имеются уникальные строки? " + unique_strings);
-
-       System.out.println();
-
-
-   }
+        System.out.println();
+    }
 }
 
--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,185 +1,189 @@
-void main() {
+package Lesson_1;
 
-    IO.println("1. ВЫВОД ASCII-ГРАФИКИ");
-    IO.println("Способ 1");
-    IO.println(
-            """
-                                         /\\\
-                    
-                       J    a  v     v  /  \\\
-                    
-                       J   a a  v   v  /_( )\\\
-                    
-                    J  J  aaaaa  V V  /      \\\
-                    
-                     JJ  a     a  V  /___/\\___\\
-                    """);
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    IO.println("Способ 2 (Text block)");
-    IO.println("""
-                     /\\
-               J    /  \\  v     v  a
-               J   /_( )\\  v   v  a a
-            J  J  /      \\  V V  aaaaa
-             JJ  /___/\\___\\  V  a     a""");
+public class VariablesTheme {
+    static void main() {
+        System.out.println("1. ВЫВОД ASCII-ГРАФИКИ");
+        System.out.println("Способ 1");
+        System.out.println(
+                """
+                                             /\\\
+                        
+                           J    a  v     v  /  \\\
+                        
+                           J   a a  v   v  /_( )\\\
+                        
+                        J  J  aaaaa  V V  /      \\\
+                        
+                         JJ  a     a  V  /___/\\___\\
+                        """);
 
-    IO.println();
-    IO.println("РАСЧЕТ СТОИМОСТИ ТОВАРА");
+        System.out.println("Способ 2 (Text block)");
+        System.out.println("""
+                         /\\
+                   J    /  \\  v     v  a
+                   J   /_( )\\  v   v  a a
+                J  J  /      \\  V V  aaaaa
+                 JJ  /___/\\___\\  V  a     a""");
 
-    IO.println("Способ 1 (float)");
-    float penPrice = 105.5f;
-    float bookPrice = 235.23f;
-    float discount = 0.11f;
+        System.out.println();
+        System.out.println("РАСЧЕТ СТОИМОСТИ ТОВАРА");
 
-    float basePrice = penPrice + bookPrice;
-    float discountSum = basePrice * discount;
+        System.out.println("Способ 1 (float)");
+        float penPrice = 105.5f;
+        float bookPrice = 235.23f;
+        float discount = 0.11f;
 
+        float basePrice = penPrice + bookPrice;
+        float discountSum = basePrice * discount;
 
-    IO.println("  Стоимость товаров без скидки: " + basePrice + " руб.");
-    IO.println("  Сумма скидки: " + discountSum + " руб.");
-    IO.println("  Стоимость товаров со скидкой: " + (basePrice - discountSum) + " руб.");
+        System.out.println("  Стоимость товаров без скидки: " + basePrice + " руб.");
+        System.out.println("  Сумма скидки: " + discountSum + " руб.");
+        System.out.println("  Стоимость товаров со скидкой: " + (basePrice - discountSum) + " руб.");
 
-    IO.println();
-    IO.println("Способ 2 (BigDecimal)");
+        System.out.println();
+        System.out.println("Способ 2 (BigDecimal)");
 
-    BigDecimal penPriceBd = new BigDecimal("105.5");
-    BigDecimal bookPriceBd = new BigDecimal("235.83");
-    BigDecimal discountBd = new BigDecimal("0.11");
+        BigDecimal penPriceBd = new BigDecimal("105.5");
+        BigDecimal bookPriceBd = new BigDecimal("235.83");
+        BigDecimal discountBd = new BigDecimal("0.11");
 
-    BigDecimal basePriceBd = penPriceBd.add(bookPriceBd).setScale(2, RoundingMode.HALF_DOWN);
-    BigDecimal discountSumBd = basePriceBd.multiply(discountBd).setScale(2, RoundingMode.HALF_DOWN);
-    BigDecimal discountPriceBd = basePriceBd.subtract(discountSumBd).setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal basePriceBd = penPriceBd.add(bookPriceBd).setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal discountSumBd = basePriceBd.multiply(discountBd).setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal discountPriceBd = basePriceBd.subtract(discountSumBd).setScale(2, RoundingMode.HALF_DOWN);
 
-    IO.println("  Стоимость товаров без скидки: " + basePriceBd + " руб.");
-    IO.println("  Сумма скидки: " + discountSumBd + " руб.");
-    IO.println("  Стоимость товаров со скидкой: " + discountPriceBd + " руб.");
+        System.out.println("  Стоимость товаров без скидки: " + basePriceBd + " руб.");
+        System.out.println("  Сумма скидки: " + discountSumBd + " руб.");
+        System.out.println("  Стоимость товаров со скидкой: " + discountPriceBd + " руб.");
 
-    IO.println();
+        System.out.println();
 
-    IO.println("3. ПЕРЕСТАНОВКА ЗНАЧЕНИЙ ЯЧЕЕК В ТАБЛИЦЕ");
+        System.out.println("3. ПЕРЕСТАНОВКА ЗНАЧЕНИЙ ЯЧЕЕК В ТАБЛИЦЕ");
 
-    int a = 2;
-    int b = 5;
+        int a = 2;
+        int b = 5;
 
-    IO.println("  Исходные значения: A = " + a + ", B = " + b);
-    IO.println("Метод: третья переменная");
-    int swap = a;
-    a = b;
-    b = swap;
-    IO.println("  Результат: A = " + a + ", B1 = " + b);
+        System.out.println("  Исходные значения: A = " + a + ", B = " + b);
+        System.out.println("Метод: третья переменная");
+        int swap = a;
+        a = b;
+        b = swap;
+        System.out.println("  Результат: A = " + a + ", B1 = " + b);
 
-    IO.println("Метод: арифметические операции");
-    a += b;
-    b = a - b;
-    a -= b;
-    IO.println("  Результат: A = " + a + ", B = " + b);
+        System.out.println("Метод: арифметические операции");
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println("  Результат: A = " + a + ", B = " + b);
 
-    IO.println("Метод: побитовая операция ^");
-    a ^= b;
-    b ^= a;
-    a ^= b;
-    IO.println("  Результат: A = " + a + ", B = " + b);
+        System.out.println("Метод: побитовая операция ^");
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        System.out.println("  Результат: A = " + a + ", B = " + b);
 
-    IO.println();
-    IO.println("\n4. ДЕКОДИРОВАНИЕ СООБЩЕНИЯ");
-    int code1055 = 1055;
-    int code1088 = 1088;
-    int code1080 = 1080;
-    int code1074 = 1074;
-    int code1077 = 1077;
-    int code1090 = 1090;
-    System.out.printf("%5d%5d%5d%5d%5d%5d%n",
-            code1055, code1088, code1080, code1074, code1077, code1090);
-    System.out.printf("%5c%5c%5c%5c%5c%5c%n",
-            code1055, code1088, code1080, code1074, code1077, code1090);
+        System.out.println("\n4. ДЕКОДИРОВАНИЕ СООБЩЕНИЯ");
+        int code1055 = 1055;
+        int code1088 = 1088;
+        int code1080 = 1080;
+        int code1074 = 1074;
+        int code1077 = 1077;
+        int code1090 = 1090;
+        System.out.printf("%5d%5d%5d%5d%5d%5d%n",
+                code1055, code1088, code1080, code1074, code1077, code1090);
+        System.out.printf("%5c%5c%5c%5c%5c%5c%n",
+                code1055, code1088, code1080, code1074, code1077, code1090);
 
-    IO.println();
-    IO.println("\n5. АНАЛИЗ КОДА ТОВАРА");
-    int productCode = 123;
-    int category = productCode / 100;
-    int subcategory = productCode / 10 % 10;
-    int packageType = productCode % 10;
+        System.out.println();
+        System.out.println("\n5. АНАЛИЗ КОДА ТОВАРА");
+        int productCode = 123;
+        int category = productCode / 100;
+        int subcategory = productCode / 10 % 10;
+        int packageType = productCode % 10;
 
-    IO.println("Код товара: " + productCode +
-            "\n  категория товара - " + category +
-            "\n  подкатегория - " + subcategory +
-            "\n  тип упаковки - " + packageType +
-            "\nКонтрольная сумма = " + (category + subcategory + packageType) +
-            "\nПроверочный код = " + (category * subcategory * packageType));
+        System.out.println("Код товара: " + productCode +
+                "\n  категория товара - " + category +
+                "\n  подкатегория - " + subcategory +
+                "\n  тип упаковки - " + packageType +
+                "\nКонтрольная сумма = " + (category + subcategory + packageType) +
+                "\nПроверочный код = " + (category * subcategory * packageType));
 
-    IO.println();
+        System.out.println();
 
-    IO.println("6. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ");
-    byte temp = Byte.MAX_VALUE;
-    System.out.printf("""
-            [Температура, °C]:
-              Исходное: %d
-              +1: %d
-              -1: %d%n
-            """, temp, ++temp, --temp);
+        System.out.println("6. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ");
+        byte temp = Byte.MAX_VALUE;
+        System.out.printf("""
+                [Температура, °C]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d%n
+                """, temp, ++temp, --temp);
 
-    short pressure = Short.MAX_VALUE;
-    System.out.printf("""
-            [Давление, kPa]:
-              Исходное: %d
-              +1: %d
-              -1: %d%n
-            """, pressure, ++pressure, --pressure);
+        short pressure = Short.MAX_VALUE;
+        System.out.printf("""
+                [Давление, kPa]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d%n
+                """, pressure, ++pressure, --pressure);
 
-    char systemStateCode = Character.MAX_VALUE;
-    System.out.printf("""
-            [Код состояния системы]:
-              Исходное: %d
-              +1: %d
-              -1: %d%n
-            """, (int) systemStateCode, (int) ++systemStateCode, (int) --systemStateCode);
+        char systemStateCode = Character.MAX_VALUE;
+        System.out.printf("""
+                [Код состояния системы]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d%n
+                """, (int) systemStateCode, (int) ++systemStateCode, (int) --systemStateCode);
 
-    int distanceCovered = Integer.MAX_VALUE;
-    System.out.printf("""
-            [Пройденное расстояние, pc]:
-              Исходное: %d
-              +1: %d
-              -1: %d%n
-            """, distanceCovered, ++distanceCovered, --distanceCovered);
+        int distanceCovered = Integer.MAX_VALUE;
+        System.out.printf("""
+                [Пройденное расстояние, pc]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d%n
+                """, distanceCovered, ++distanceCovered, --distanceCovered);
 
-    long missionElapsedTime = Long.MAX_VALUE;
-    System.out.printf("""       
-            [Время с момента старта, DDD]:
-              Исходное: %d
-              +1: %d
-              -1: %d
-            """, missionElapsedTime, ++missionElapsedTime, --missionElapsedTime);
+        long missionElapsedTime = Long.MAX_VALUE;
+        System.out.printf("""       
+                [Время с момента старта, DDD]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d
+                """, missionElapsedTime, ++missionElapsedTime, --missionElapsedTime);
 
-    IO.println();
-    IO.println("ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА");
+        System.out.println();
+        System.out.println("ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА");
 
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-    long startNanos = System.nanoTime();
-    LocalTime startTime = LocalTime.now();
-    try {
-        Thread.sleep(59);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+        long startNanos = System.nanoTime();
+        LocalTime startTime = LocalTime.now();
+        try {
+            Thread.sleep(59);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long endNanos = System.nanoTime();
+        LocalTime endTime = LocalTime.now();
+
+        double elapsedSeconds = (endNanos - startNanos) / 1_000_000_000.0;
+        String formattedTime = String.format("%.3f", elapsedSeconds).replace('.', ',');
+        System.out.printf("""
+                        | Старт проверки | %s |
+                        +----------------+--------------+
+                        | Финиш проверки | %s |
+                        +----------------+--------------+
+                        | Время работы   | %s сек    |
+                        """,
+                dtf.format(startTime),
+                dtf.format(endTime),
+                formattedTime
+        );
     }
-
-    long endNanos = System.nanoTime();
-    LocalTime endTime = LocalTime.now();
-
-    double elapsedSeconds = (endNanos - startNanos) / 1_000_000_000.0;
-    String formattedTime = String.format("%.3f", elapsedSeconds).replace('.', ',');
-    System.out.printf("""
-                    | Старт проверки | %s |
-                    +----------------+--------------+
-                    | Финиш проверки | %s |
-                    +----------------+--------------+
-                    | Время работы   | %s сек    |
-                    """,
-            dtf.format(startTime),
-            dtf.format(endTime),
-            formattedTime
-    );
-
-
 }
 
 
